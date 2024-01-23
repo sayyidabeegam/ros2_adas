70d7b02ea5ad1ca49f153039c096655525fbe695
sayyidabeegam <sayyidabeegam@gmail.com>
2024-01-23 22:01:29 +0530

    Adding launch files

---
+++ lane_detection_test_node/launch/lane_detection_test_launch.py
@@ -0,0 +1,13 @@
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='lane_detection_test_node',  # Replace with the actual name of your ROS2 package
+            executable='lane_detection_test_node',
+            name='lane_detection_test_node',
+        ),
+    ])
+
